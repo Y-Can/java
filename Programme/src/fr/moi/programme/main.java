1595903f3d59781bde62a32854d11a372ac28e10
Can <71851732+Y-Can@users.noreply.github.com>
2021-09-19 20:47:59 +0200

    Add files via upload

---
+++ Programme/src/fr/moi/programme/main.java
@@ -0,0 +1,22 @@
+package fr.moi.programme;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int note1 = 50;
+		int note2 = 5;
+		int note3 = 44;
+		
+		int calcul = (note1 + note2 + note3) / 3;
+		
+		if (calcul> 20) {
+		System.out.println("C'est supérieur a 20");
+	}
+		else {
+			System.out.println("C'est inférieur à 20");
+		}
+
+}
+
+}
